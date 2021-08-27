fd205b5fd80de55bac6568cd2a2fbcc8d598f4b9
yoichi <itiyou14@gmail.com>
2021-08-27 13:24:33 +0900

    ランドマーク等から緯度、軽度を取得。詳細な住所を出力

--- restaurants-searcher.js
+++ restaurants-searcher.js
@@ -1,1 +1,37 @@
 const { Client } = require('@googlemaps/google-maps-services-js')
+const address = require('minimist')(process.argv.slice(2))._[0]
+
+async function getLatLngFromAboutAddress (address) {
+  const client = new Client({})
+  const res = await client
+    .geocode({
+      params: {
+        address: address,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+        language: 'ja'
+      }
+    })
+  const lat = res.data.results[0].geometry.location.lat
+  const lng = res.data.results[0].geometry.location.lng
+  const latLng = `${lat},${lng}`
+  return latLng
+}
+
+async function serchPoint (latLng) {
+  const client = new Client({})
+  const res = await client
+    .geocode({
+      params: {
+        latlng: latLng,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+        language: 'ja'
+      }
+    })
+  console.log(res.data.results[0].formatted_address)
+}
+
+async function main (address) {
+  const latLng = await getLatLngFromAboutAddress(address)
+  serchPoint(latLng)
+}
+main(address)
